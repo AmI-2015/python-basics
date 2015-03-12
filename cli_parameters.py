b7847bd219c2fe02efafb2a8ed4bd890ec36e1e0
Luigi <luigi.derussis@polito.it>
2015-03-12 12:25:49 +0100

    remove double space

--- cli_parameters.py
+++ cli_parameters.py
@@ -22,7 +22,7 @@ from sys import argv
 if __name__ == '__main__':
     script, first, second, third = argv #argv unpacking
     
-    print 'The script is called: ', script
-    print 'Your first variable is: ', first 
-    print 'Your second variable is: ', second
-    print 'Your third variable is: ', third 
+    print 'The script is called:', script
+    print 'Your first variable is:', first 
+    print 'Your second variable is:', second
+    print 'Your third variable is:', third 
